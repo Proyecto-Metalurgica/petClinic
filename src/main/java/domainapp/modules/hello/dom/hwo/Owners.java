a17533307a3af25688e2c4b36004568b60f4adce
Caro <carobaeza85@gmail.com>
2020-05-04 19:38:12 -0300

    Se renombro clase helloWorld y se actualilizaron sus dependencias
    Se resuelve issue 020 #2

--- src/main/java/domainapp/modules/hello/dom/hwo/Owners.java
+++ src/main/java/domainapp/modules/hello/dom/hwo/Owners.java
@@ -14,19 +14,19 @@ import org.apache.isis.applib.annotation.SemanticsOf;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.apache.isis.persistence.jdo.applib.services.IsisJdoSupport_v3_2;
 
-import domainapp.modules.hello.dom.hwo.QHelloWorldObject;
+
 import domainapp.modules.hello.types.Name;
 
 @DomainService(
         nature = NatureOfService.VIEW,
-        objectType = "hello.HelloWorldObjects"
+        objectType = "hello.Owners"
         )
-public class HelloWorldObjects {
+public class Owners {
 
     private final RepositoryService repositoryService;
     private final IsisJdoSupport_v3_2 isisJdoSupport;
 
-    public HelloWorldObjects(
+    public Owners(
             final RepositoryService repositoryService,
             final IsisJdoSupport_v3_2 isisJdoSupport) {
         this.repositoryService = repositoryService;
@@ -35,9 +35,9 @@ public class HelloWorldObjects {
 
     @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
     @ActionLayout(promptStyle = PromptStyle.DIALOG_MODAL)
-    public HelloWorldObject create(
+    public Owner create(
             @Name final String name) {
-        return repositoryService.persist(new HelloWorldObject(name));
+        return repositoryService.persist(new Owner(name));
     }
     public String default0Create() {
         return "Hello World!";
@@ -46,10 +46,10 @@ public class HelloWorldObjects {
 
     @Action(semantics = SemanticsOf.SAFE)
     @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
-    public List<HelloWorldObject> findByName(
+    public List<Owner> findByName(
             @Name final String name) {
-        JDOQLTypedQuery<HelloWorldObject> q = isisJdoSupport.newTypesafeQuery(HelloWorldObject.class);
-        final QHelloWorldObject cand = QHelloWorldObject.candidate();
+        JDOQLTypedQuery<Owner> q = isisJdoSupport.newTypesafeQuery(Owner.class);
+        final QOwner cand = QOwner.candidate();
         q = q.filter(
                 cand.name.indexOf(q.stringParameter("name")).ne(-1)
                 );
@@ -58,8 +58,8 @@ public class HelloWorldObjects {
     }
 
     @Action(semantics = SemanticsOf.SAFE, restrictTo = RestrictTo.PROTOTYPING)
-    public List<HelloWorldObject> listAll() {
-        return repositoryService.allInstances(HelloWorldObject.class);
+    public List<Owner> listAll() {
+        return repositoryService.allInstances(Owner.class);
     }
 
 
